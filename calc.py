369e8a8053ea8d3ab85998c22671ab4e34d7400d
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-02-05 13:48:41 -0500

    first commit

---
+++ calc.py
@@ -0,0 +1,16 @@
+# imports
+
+# globals
+
+# functions
+def print_menu():
+    print("--------------")
+    print("[1] Sum")
+    print("[2] Subtract")
+    print("----------------")
+
+
+# plain instructions
+print_menu()
+opt = input("Select an option: ")
+print(opt)
---
+++ intro.py
@@ -0,0 +1,43 @@
+print("hiya world!")
+
+
+# variables
+name = "Gary"
+last_name = "LastName"
+total = 3.50
+age = 101
+boolean = True  # or false = ALWAYS Capped
+
+# if / else statements
+# indentation level matters
+if age < 100:
+    print("dang, yer old!")
+    print("Inside the if")
+    print("me as well")
+else:
+    print("its ok, dude, no worries!")
+
+print("outside the if")
+
+
+# Functions
+def say_hello():
+    print("say hiya!")
+
+
+# call function
+say_hello()
+say_hello()
+say_hello()
+
+
+# math
+print(1 + 2)
+print(9 - 1)
+print(3 * 4)
+print(100 / 10)
+
+
+# test
+print("hello my name is Gary" + name)
+print("and Im " + str(age) + "years old")
